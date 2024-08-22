b2bcc50163a04f101b012733a13c744fcb09d19d
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2024-08-21 21:32:44 -0300

    finalizado

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <img src="/src/assets/user.png" alt="" className={styles.footerLogo}/>
+            <img src="./src/assets/user.png" alt="" className={styles.footerLogo}/>
             <div className={styles.footerContent}>
                 <h3 className={styles.footerTitle}>Thank you!</h3>
                 <p className={styles.footerText}>Follow me on my social networks and let's talk </p>
